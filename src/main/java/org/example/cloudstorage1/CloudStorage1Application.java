57c796a8eff20c117a1691e7a8cb30fb31af0e8a
Sabr <sabo98@mail.ru>
2025-12-16 16:55:52 +0500

    first commit

---
+++ src/main/java/org/example/cloudstorage1/CloudStorage1Application.java
@@ -0,0 +1,13 @@
+package org.example.cloudstorage1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudStorage1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudStorage1Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cloudstorage1/CloudStorage1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cloudstorage1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStorage1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
