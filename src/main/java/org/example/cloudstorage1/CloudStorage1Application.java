6b9a434ad369b1f50fc6f6c4b9a924c05eb9ce3b
Sabr <sabo98@mail.ru>
2025-12-16 18:07:24 +0500

    feat: docker compose yml, postgres

--- src/main/java/org/example/cloudstorage1/CloudStorage1Application.java
+++ src/main/java/org/example/cloudstorage1/CloudStorage1Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CloudStorage1Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(CloudStorage1Application.class, args);
     }
-
 }
