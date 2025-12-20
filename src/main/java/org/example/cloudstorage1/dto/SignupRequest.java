bf19c8c0743147505c0720c1d83432d4fe2dd71e
Sabr <sabo98@mail.ru>
2025-12-20 11:22:16 +0500

    feat: valid для логин и пароля

--- src/main/java/org/example/cloudstorage1/dto/SignupRequest.java
+++ src/main/java/org/example/cloudstorage1/dto/SignupRequest.java
@@ -1,4 +1,16 @@
 package org.example.cloudstorage1.dto;
 
-public record SignupRequest(String username, String password) {
-}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record SignupRequest(
+        @NotBlank(message = "Username is required")
+        @Size(min = 4, max = 12, message = "Username must be between 4 and 12 characters long")
+        @Pattern(regexp = "^[a-zA-Z0-9_]+$", message = "Username can only contain letters, numbers and underscore")
+        String username,
+
+        @NotBlank(message = "Password is required")
+        @Size(min = 4, max = 20, message = "Password must be between 4 and 20 characters long")
+        String password
+) {}
