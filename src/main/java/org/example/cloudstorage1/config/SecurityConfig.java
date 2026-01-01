a9d3670142238df2791d08343b206e9c7ee09194
Sabr <sabo98@mail.ru>
2026-01-01 19:54:49 +0500

    refac:

--- src/main/java/org/example/cloudstorage1/config/SecurityConfig.java
+++ src/main/java/org/example/cloudstorage1/config/SecurityConfig.java
@@ -1,9 +1,6 @@
 package org.example.cloudstorage1.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletResponse;
-import org.example.cloudstorage1.dto.ErrorMessage;
-import org.example.cloudstorage1.dto.ErrorResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
