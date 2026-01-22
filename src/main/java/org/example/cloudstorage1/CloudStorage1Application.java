cb1286eaaba2cdc0ebf306534cb59b5f195edb5c
Syrym.Sabyrzhan <syrym.sabyrzhan@astanait.edu.com>
2026-01-22 13:54:31 +0500

    fixes

--- src/main/java/org/example/cloudstorage1/CloudStorage1Application.java
+++ src/main/java/org/example/cloudstorage1/CloudStorage1Application.java
@@ -2,8 +2,10 @@ package org.example.cloudstorage1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 1800) // 30 минут
 public class CloudStorage1Application {
 
     public static void main(String[] args) {
---
+++ src/main/java/org/example/cloudstorage1/config/OpenApiConfig.java
@@ -0,0 +1,34 @@
+package org.example.cloudstorage1.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Cloud Storage API")
+                        .version("1.0")
+                        .description("File management system API")
+                )
+                .addSecurityItem(new SecurityRequirement()
+                        .addList("cookieAuth")
+                )
+                .components(new Components()
+                        .addSecuritySchemes("cookieAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.APIKEY)
+                                        .in(SecurityScheme.In.COOKIE)
+                                        .name("JSESSIONID")
+                        )
+                );
+    }
+}
